59ac18f22383e00d8abd8062a9e13461902de7ec
Tristan Schrader <t0rdos@pm.me>
2024-03-19 00:57:15 -0700

    feat: add jupyterlab dev dep into devShell
    extracted `groupDeps` function to get a list of derivations from a group
    in the python project's pyproject.toml

--- projects/nnfs/python/default.nix
+++ projects/nnfs/python/default.nix
@@ -6,31 +6,38 @@
     ...
   }: let
     inherit (builtins) getAttr attrValues;
-    inherit (lib.trivial) pipe;
-  in {
-    imports = [dream2nix.modules.dream2nix.WIP-python-pdm];
-    deps = {nixpkgs, ...}: {
-      inherit (nixpkgs.python312.pkgs) python pytestCheckHook;
-    };
-    mkDerivation.src = ./.;
-    mkDerivation.nativeCheckInputs = lib.lists.flatten [
-      config.deps.pytestCheckHook
-      (pipe config.groups.test.packages [
+    inherit (lib.trivial) concat pipe;
+    inherit (lib.lists) flatten;
+    groupDeps = group:
+      pipe config.groups.${group}.packages [
         attrValues
         (map (pkg:
           pipe pkg [
             attrValues
             (map (getAttr "public"))
           ]))
-      ])
-    ];
+        flatten
+      ];
+  in {
+    imports = [dream2nix.modules.dream2nix.WIP-python-pdm];
+    deps = {nixpkgs, ...}: {
+      inherit (nixpkgs.python312.pkgs) python pytestCheckHook;
+    };
+    mkDerivation.src = ./.;
+    mkDerivation.nativeCheckInputs = concat (groupDeps "test") [config.deps.pytestCheckHook];
     pdm.pyproject = ./pyproject.toml;
     pdm.lockfile = ./pdm.lock;
     # The existing devShell doesn't work so we must forcefully override
     # config.deps.pdm should be listed after python so its own python doesn't override our package set
     public.devShell = lib.mkForce (config.deps.mkShell {
       packages = [
-        (config.deps.python.withPackages (ps: with config.mkDerivation; lib.trivial.concat propagatedBuildInputs nativeCheckInputs))
+        (config.deps.python.withPackages (ps:
+          with config.mkDerivation;
+            flatten [
+              propagatedBuildInputs
+              nativeCheckInputs
+              (groupDeps "dev")
+            ]))
         config.deps.pdm
       ];
     });
