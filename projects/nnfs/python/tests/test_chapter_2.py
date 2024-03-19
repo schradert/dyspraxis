58324468bef550ef3439a319caaf4901e82f7dc6
Tristan Schrader <t0rdos@pm.me>
2024-03-18 23:29:58 -0700

    feat: add test for multiple samples

--- projects/nnfs/python/tests/test_chapter_2.py
+++ projects/nnfs/python/tests/test_chapter_2.py
@@ -1,7 +1,7 @@
 from math import isclose
 from numpy import allclose, array, dot
 
-def test_three_neurons():
+def test_three_neurons_one_sample_list():
     inputs = [1, 2, 3, 2.5]
     weights = [
         [0.2, 0.8, -0.5, 1],
@@ -18,6 +18,36 @@ def test_three_neurons():
     for r, o in zip(result, outputs):
         assert isclose(r, o, rel_tol=1e-9)
 
-    outputs_np = array(outputs)
-    result_np = dot(weights, inputs) + biases
-    assert allclose(result_np, outputs_np, rtol=1e-9)
+def test_three_neurons_one_sample_numpy():
+    inputs = array([1, 2, 3, 2.5])
+    weights = array([
+        [0.2, 0.8, -0.5, 1],
+        [0.5, -0.91, 0.26, -0.5],
+        [-0.26, -0.27, 0.17, 0.87],
+    ])
+    biases = array([2, 3, 0.5])
+    outputs = array([4.8, 1.21, 2.385])
+
+    result = dot(weights, inputs) + biases
+    assert allclose(result, outputs, rtol=1e-9)
+
+def test_three_neurons_three_samples_numpy():
+    inputs = array([
+        [1.0, 2.0, 3.0, 2.5],
+        [2.0, 5.0, -1.0, 2.0],
+        [-1.5, 2.7, 3.3, -0.8],
+    ])
+    weights = array([
+        [0.2, 0.8, -0.5, 1.0],
+        [0.5, -0.91, 0.26, -0.5],
+        [-0.26, -0.27, 0.17, 0.87],
+    ])
+    biases = array([2.0, 3.0, 0.5])
+    outputs = array([
+        [4.8, 1.21, 2.385],
+        [8.9, -1.81, 0.2],
+        [1.41, 1.051, 0.026],
+    ])
+
+    result = dot(inputs, weights.T) + biases
+    assert allclose(result, outputs, rtol=1e-9)
